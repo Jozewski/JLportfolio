1d53eb09283b790173c3b0808b8a75bb0aa1a968
Jozewski <joanneliszewski@gmail.com>
2025-08-26 18:17:13 -0700

    chore(config): update layout and Next.js config for improved setup.

--- next.config.mjs
+++ next.config.mjs
@@ -26,7 +26,7 @@ export default {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; frame-src https://www.youtube.com https://www.youtube-nocookie.com; script-src 'self' 'unsafe-inline' https://www.youtube.com https://www.youtube-nocookie.com; connect-src 'self'; img-src 'self' data: https://i.ytimg.com; style-src 'self' 'unsafe-inline';"
+            value: "default-src 'self'; frame-src https://www.youtube.com https://www.youtube-nocookie.com; script-src 'self' 'unsafe-inline' https://www.youtube.com https://www.youtube-nocookie.com https://www.gstatic.com https://s.ytimg.com; connect-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://i.ytimg.com https://s.ytimg.com; img-src 'self' data: https://i.ytimg.com https://s.ytimg.com; style-src 'self' 'unsafe-inline';"
           }
         ]
       }
