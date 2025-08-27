4c4fe4301f7d2af90d8c9b0d45fab05234d47dee
Jozewski <joanneliszewski@gmail.com>
2025-08-27 05:20:30 -0700

    refactor in order to allow inline videos to play

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  allowedDevOrigins: [
-    'http://localhost:3000',
-    'http://192.168.0.8:3000',
-  ],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-src 'self' https://www.youtube.com https://youtube.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com; object-src 'none'; media-src 'self' https://www.youtube.com;"
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
